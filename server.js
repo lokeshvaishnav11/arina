0e09ba35a2d028bb4bf037a2324e280717ee64d4
admin <admin@admins-MacBook-Pro.local>
2026-08-08 16:19:03 +0530

    first commit

--- server.js
+++ server.js
@@ -37,6 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/admin', (req,res)=> {
   res.sendFile(path.join(__dirname,'public','admin.html'));
 });
+app.get('/login', (req,res)=> {
+  res.sendFile(path.join(__dirname,'public','login.html'));
+});
 
 app.get('*', (req,res)=> {
   res.sendFile(path.join(__dirname,'public','index.html'));
